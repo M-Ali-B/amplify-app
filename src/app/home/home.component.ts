ef4a5052065980fb725d282f59253f387c6a28df
Ali Baba <alibabatorjan001@gmail.com>
2020-04-06 20:25:54 +0500

    signout implemented by manually removing local storage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,29 +10,27 @@ const auth = Auth;
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    constructor(private router: Router) {}
     ngOnInit(): void {
     }
-
+ getWindow() {
+     return window;
+ }
     onSignOut() {
-        // auth.signOut()
-        //     .then(data => console.log(data))
-        //     .catch(err => console.log(err));
-
         // By doing this, you are revoking all the auth tokens(id token, access token and refresh token)
         // which means the user is signed out from all the devices
         // Note:ss although the tokens are revoked, the AWS credentials will remain valid until they expire (which by default is 1 hour)
 
-        auth.currentAuthenticatedUser()
+
+        auth.signOut({global: true})
             .then(data => console.log(data))
             .catch(err => console.log(err));
-        // console.log(auth);
-        auth.signOut({ global: true })
-            .then(data => console.log(data))
-            .catch(err => console.log(err));
-        auth.currentAuthenticatedUser()
-             .then(data => console.log(data))
-                .catch(err => console.log(err));
-        //console.log(auth);
+
+        this.getWindow().localStorage.clear();
+        this.router.navigateByUrl('/')
+
     }
+
+
 }
 
